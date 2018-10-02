f3b79cbc708e093a49c4e8360e80f510f720d898
Mikael Strid <mikael.strid@outlook.com>
2018-10-02 22:01:26 +0200

    Renamed the function.

--- Smarthome.Functions.Receivers/TemperatureHumidityFunction.cs
+++ Smarthome.Functions.Receivers/TemperatureHumidityFunction.cs
@@ -1,20 +1,17 @@
-
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Smarthome.Functions.Receivers
 {
-    public static class Function1
+    public static class TemperatureHumidityFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("TemperatureHumidity")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
